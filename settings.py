3371733bb75df021f6567464b71661d5a15a5439
saintoleg89 <119620553+saintoleg89@users.noreply.github.com>
2023-06-10 14:16:03 +0300

    Add files via upload

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@
 Переменные, необходимые для работы бота
 """
 
-vk_token = 'vk1.a.frKqbm4g-sHMwlpa4h7YEGMQYFXDVSj3928I5WZTjqLeaLNSW3RbN6MsW1NEEsbs8JxmZCHdKabsKrEMWlmjkeEMdCHTchBcIAwXRfu0vlJcepJlFeaQj5r-6yi2vVxmcrbFLtTXWoNTKbfx5aF-a8twSje4Fj55ATwouF_6tCtFFA8jk2moO86uR8kZccUXFmIf3Azf7VMAO_wj1stMWg' #токен группы
-search_token = 'vk1.a.CV135w78EbUOokEAdHqTCWXYIFbvPF1nxbQFga-tPEq-a2JqMxd7Xbr2w1aR8Uza_NcwXD0o4B4zB0g-2QYTnLAhIZP4cayg-haSzB6TaWqxJsJmpBTEeM3C3Q_Q7n4Oz51MFE1DHEsJdOf9OBW9-7vi2lNDrclyGuIrfxe6hrk-kLqN2em7jOJ7K-EpXSFB' #токен пользователя
+vk_token = '' #токен группы
+search_token = '' #токен пользователя
 
 db_pass = 'bot' #пароль базы данных
 db_admin = 'bot' #администратор базы данных
